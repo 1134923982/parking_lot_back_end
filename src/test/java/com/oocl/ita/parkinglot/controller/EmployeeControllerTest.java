f764b826f634cd5e1f83b16c273fc2429f658e93
ZHANGKH <KhaiZhang@163.com>
2019-07-28 14:30:51 +0800

    [Khai][feat :implement get employee all parkinglots / employeeController & test should return employee all parkinglots / EmployeeController]

---
+++ src/test/java/com/oocl/ita/parkinglot/controller/EmployeeControllerTest.java
@@ -0,0 +1,65 @@
+package com.oocl.ita.parkinglot.controller;
+
+import com.oocl.ita.parkinglot.model.ParkingLot;
+import com.oocl.ita.parkinglot.service.EmployeeService;
+import com.oocl.ita.parkinglot.service.ServiceImpl.EmployeeServiceImpl;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class EmployeeControllerTest {
+
+    @MockBean
+    private EmployeeService employeeService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void should_return_employee_all_parkinglots_when_employee_is_exist () throws Exception{
+        ParkingLot firstParkingLot = new ParkingLot();
+        ParkingLot secondParkingLot = new ParkingLot();
+        ParkingLot thirdParkingLot = new ParkingLot();
+        ArrayList<ParkingLot> parkingLots = new ArrayList<>();
+        parkingLots.add(firstParkingLot);
+        parkingLots.add(secondParkingLot);
+        parkingLots.add(thirdParkingLot);
+
+        when(employeeService.getEmployeeAllParkingLots(anyString())).thenReturn(parkingLots);
+
+        mockMvc.perform(get("/employees/{id}/parking-lots","1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(3));
+    }
+
+    @Test
+    public void should_return_employee_all_parkinglots_when_employee_is_not_exist () throws Exception{
+
+        when(employeeService.getEmployeeAllParkingLots(anyString())).thenReturn(null);
+
+        mockMvc.perform(get("/employees/{id}/parking-lots","0"))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+
+    }
+
+}
