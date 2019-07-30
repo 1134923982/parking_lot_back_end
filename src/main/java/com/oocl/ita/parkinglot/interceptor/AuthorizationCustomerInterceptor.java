9af78b945481d454a9a0a94076bd41e9a5862041
2228787638 <2228787638@qq.com>
2019-07-30 12:56:46 +0800

    [Gordon][feat: implement customer's interceptor and set it into SecurityCustomerUtils]

---
+++ src/main/java/com/oocl/ita/parkinglot/interceptor/AuthorizationCustomerInterceptor.java
@@ -0,0 +1,94 @@
+package com.oocl.ita.parkinglot.interceptor;
+
+import com.auth0.jwt.internal.com.fasterxml.jackson.databind.ObjectMapper;
+import com.oocl.ita.parkinglot.annotation.Auth;
+import com.oocl.ita.parkinglot.model.Customer;
+import com.oocl.ita.parkinglot.model.Employee;
+import com.oocl.ita.parkinglot.utils.SecurityCustomerUtils;
+import com.oocl.ita.parkinglot.utils.SecurityUtils;
+import com.oocl.ita.parkinglot.vo.ResultVO;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import static com.oocl.ita.parkinglot.enums.CodeMsgEnum.TOKENCAN_EXCEPTION;
+import static com.oocl.ita.parkinglot.enums.CodeMsgEnum.TOKEN_EXCEPTION;
+
+/**
+ * @author Gordon
+ */
+public class AuthorizationCustomerInterceptor implements HandlerInterceptor {
+    private static final String[] IGNORE_URI = {"/register","/login"};
+    /**
+     * 该方法将在整个请求完成之后执行， 主要作用是用于清理资源的，
+     * 该方法也只能在当前Interceptor的preHandle方法的返回值为true时才会执行。
+     */
+    @Override
+    public void afterCompletion(HttpServletRequest request,
+                                HttpServletResponse response, Object handler, Exception exception)
+            throws Exception {
+    }
+    /**
+     * 该方法将在Controller的方法调用之后执行， 方法中可以对ModelAndView进行操作 ，
+     * 该方法也只能在当前Interceptor的preHandle方法的返回值为true时才会执行。
+     */
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response,
+                           Object handler, ModelAndView mv) throws Exception {
+    }
+
+    /**
+     * preHandle方法是进行处理器拦截用的，该方法将在Controller处理之前进行调用，
+     * 该方法的返回值为true拦截器才会继续往下执行，该方法的返回值为false的时候整个请求就结束了。
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+                             Object obeject) throws Exception {
+        String servletPath =request.getServletPath();
+        for(String ignoreRoute:IGNORE_URI){
+            if(servletPath.contains(ignoreRoute)) {
+                return true;
+            }
+        }
+        String token = getRequestToken(request);
+        SecurityCustomerUtils.setToken(token);
+        Customer customer = SecurityCustomerUtils.getCustomer();
+        if (customer == null) {
+            ObjectMapper mapper = new ObjectMapper();
+            String json = mapper.writeValueAsString(ResultVO.error(TOKEN_EXCEPTION));
+            this.returnJson(response, json);
+        }else{
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 获取请求的token(header中的token)
+     * @param httpRequest 请求
+     * @return token
+     */
+    private String getRequestToken(HttpServletRequest httpRequest){
+        String token = httpRequest.getHeader("token");
+        return token;
+    }
+
+    private void returnJson(HttpServletResponse response, String json) throws Exception{
+        PrintWriter writer = null;
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=utf-8");
+        try {
+            writer = response.getWriter();
+            writer.print(json);
+        } finally {
+            if (writer != null) {
+                writer.close();
+            }
+        }
+    }
+}
--- src/test/java/com/oocl/ita/parkinglot/service/impl/CustomerServiceImplTest.java
+++ src/test/java/com/oocl/ita/parkinglot/service/impl/CustomerServiceImplTest.java
@@ -3,6 +3,7 @@ package com.oocl.ita.parkinglot.service.impl;
 import com.oocl.ita.parkinglot.exception.ParkingLotException;
 import com.oocl.ita.parkinglot.model.Customer;
 import com.oocl.ita.parkinglot.model.Orders;
+import com.oocl.ita.parkinglot.repository.CustomerRepository;
 import com.oocl.ita.parkinglot.repository.OrdersRepository;
 import com.oocl.ita.parkinglot.service.CustomerService;
 import org.junit.Ignore;
@@ -26,6 +27,8 @@ import static org.mockito.Mockito.when;
 public class CustomerServiceImplTest {
     @MockBean
     private OrdersRepository ordersRepository;
+    @MockBean
+    private CustomerRepository customerRepository;
     @Autowired
     private CustomerService customerService;
     @Test
@@ -58,14 +61,17 @@ public class CustomerServiceImplTest {
         assertEquals(customerHistoryOrdersByCustomerId.size(),ordersList.size());
     }
 
-    @Ignore
     @Test
     public void should_return_a_new_orders_when_input_a_orders_and_has_no_unfinish_orders(){
+        Customer customer = new Customer();
         Orders orders = new Orders();
         orders.setStatus(0);
+        orders.setCustomer(customer);
         List<Orders> list = new ArrayList<>();
         when(ordersRepository.findByCustomer_IdAndStatusIsUnFinish(anyString()))
                 .thenReturn(list);
+        when(customerRepository.findById(anyString()))
+                .thenReturn(java.util.Optional.ofNullable(customer));
         when(ordersRepository.save(orders))
                 .thenReturn(orders);
         assertEquals(customerService.createCustomerOrders("1",orders).getStatus(),orders.getStatus());
